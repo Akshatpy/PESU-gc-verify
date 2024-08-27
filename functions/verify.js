e41be5765c301242b97a829b89ddd44cc9c99a8b
Akshatpy <akshatomega@gmail.com>
2024-08-27 22:41:58 +0530

    Updated index.html

--- functions/verify.js
+++ functions/verify.js
@@ -1,30 +1,21 @@
 const axios = require('axios');
-
-// Define the API handler
 exports.handler = async function(event, context) {
-  // Parse the incoming request body
   const { username, password } = JSON.parse(event.body);
-
   try {
-    // Make a POST request to the PESU authentication API
     const response = await axios.post('https://pesu-auth.onrender.com/authenticate', {
       username: username,
       password: password,
       profile: true
     });
-
-    // Check the response from PESU API
     if (response.data.status && response.data.message === "Login successful.") {
-      // If login is successful, return the WhatsApp invite link
       return {
         statusCode: 200,
         body: JSON.stringify({
           message: "SRN verified successfully!",
-          inviteLink: "https://chat.whatsapp.com/invite/YourInviteLink" // Replace with your actual invite link
+          inviteLink: process.env.WHATSAPP_INVITE_LINK 
         })
       };
     } else {
-      // If login fails, return an error message
       return {
         statusCode: 400,
         body: JSON.stringify({ message: "Failed to verify SRN. Please check the details." })
